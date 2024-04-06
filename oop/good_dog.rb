04f350cb2d8b9f847064174a577ab772cb5f44f5
Ogola Ambala <66788503+OgolaAmbala@users.noreply.github.com>
2024-04-06 09:40:24 +0100

    completed

--- oop/good_dog.rb
+++ oop/good_dog.rb
@@ -1,26 +1,31 @@
 # frozen_string_literal: true
 
-module Speak
-  def speak(sound)
-    puts "#{sound}"
+class GoodDog
+  attr_accessor :name, :height, :weight
+
+  def initialize(n, h, w)
+    @name = n
+    @height = h
+    @weight = w
+  end
+
+  def speak
+    "#{name} says Arf!"
+  end
+
+  def change_info(n, h, w)
+    self.name = n
+    self.height = h
+    self.weight = w
+  end
+
+  def info
+    "#{name} weighs #{weight} and is #{height} tall."
   end
 end
 
-class GoodDog
-  include Speak
-end
+sparky = GoodDog.new('Sparky', '12 inches', '10 lbs')
+puts sparky.info
 
-class HumanBeing
-  include Speak
-end
-
-sparky = GoodDog.new
-sparky.speak('Arf')
-bob = HumanBeing.new
-bob.speak('Hello')
-
-# puts '---GoodDog ancestors---'
-# puts GoodDog.ancestors
-# puts ''
-# puts '---HumanBeing ancestors---'
-# puts HumanBeing.ancestors
+sparky.change_info('Spartacus', '24 inches', '45 lbs')
+puts sparky.info
